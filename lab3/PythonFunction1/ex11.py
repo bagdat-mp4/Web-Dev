8d822ff4078a59ca37820a2f18bbf80c7318cd7e
bagdat-mp4 <150347909+bagdat-mp4@users.noreply.github.com>
2026-01-23 15:59:32 +0500

    Delete lab3 directory

--- lab3/PythonFunction1/ex11.py
+++
@@ -1,9 +0,0 @@
-# Палиндромды тексеру функциясы
-def is_palindrome(s):
-    s = s.replace(" ", "").lower()  # Бос орындарды жою және барлық әріптерді кіші әріпке түрлендіру
-    return s == s[::-1]  # Жолды керісінше тексеру
-
-# Мысал қолдану:
-print(is_palindrome("madam"))  # Шығару: True
-print(is_palindrome("A man a plan a canal Panama"))  # Шығару: True
-print(is_palindrome("hello"))  # Шығару: False
